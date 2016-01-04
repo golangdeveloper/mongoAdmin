f605f29259699ebf5e56a74890a25bee2a63b471
timeloveboy <734991033@qq.com>
2016-01-04 17:55:30 +0800

    Signed-off-by: timeloveboy <734991033@qq.com>

--- router.go
+++ router.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
-	"moetemplate"
+
+	"github.com/golangdeveloper/moetemplate"
 )
 
-func handle_index(w http.ResponseWriter, r *http.Request){
-	var out=moetemplate.Ok() 
+func handle_index(w http.ResponseWriter, r *http.Request) {
+	var mainpage string
+	buff, err := ioutil.ReadFile(root + "/views/" + "index.html")
+	if err == nil {
+		mainpage = string(buff)
+	}
+	var hp map[string]string
+	hp["header"] = "<p>nihao</>"
+
+	var cs map[string]string
+	cs["title"] = "你好"
+
+	var out string
+	out = moetemplate.Render(mainpage, hp, cs)
 	w.Write([]byte(out))
 }
